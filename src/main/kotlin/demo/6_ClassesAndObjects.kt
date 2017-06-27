29b0e03708288b84707d5a50d090454357c36ecc
lzp <lzp@dainihai.cn>
2017-06-27 17:32:39 +0800

    add 6 kotlin 属性 接口 可见性

--- src/main/kotlin/demo/6_ClassesAndObjects.kt
+++ src/main/kotlin/demo/6_ClassesAndObjects.kt
@@ -37,3 +37,76 @@ class DontCreateMe private constructor() {}
 class Customer3(val customerName: String = "")
 
 
+
+open class Base(p: Int)
+
+class Derived(p: Int) : Base(p)
+
+class Test(){
+    val size: Int = 1
+    val isEmpty: Boolean
+        get() = this.size == 0
+    var stringRepresentation: String
+        get() = this.toString()
+        set(v) {
+//            setDataFromString(v) // 解析字符串并赋值给其他属性
+        }
+}
+
+
+interface MyInterface {
+    val prop: Int // 抽象的
+    val propertyWithImplementation:String get() = "sadf"
+    fun bar()
+    fun foo(){}
+}
+class Child : MyInterface {
+    override val prop: Int = 345
+    override fun bar() {}
+}
+
+interface A {
+    fun foo() { print("A") }
+    fun bar()
+}
+interface B {
+    fun foo() { print("B") }
+    fun bar() { print("bar") }
+}
+class C : A {
+    override fun bar() { print("bar") }
+}
+class D : A, B {
+    override fun foo() {
+        super<A>.foo()
+        super<B>.foo()
+    }
+    override fun bar() {
+        super<B>.bar()
+    }
+}
+
+open class Outer {
+    private val a = 1
+    protected open val b = 2
+    internal val c = 3
+    val d = 4  // 默认 public
+
+    protected class Nested {
+        public val e: Int = 5
+    }
+}
+class Subclass : Outer() {
+    // a 不可见
+    // b、c、d 可见
+    // Nested 和 e 可见
+    override val b = 5   // “b”为 protected
+}
+class Unrelated(o: Outer) {
+    // o.a、o.b 不可见
+    // o.c 和 o.d 可见（相同模块）
+    // Outer.Nested 不可见，Nested::e 也不可见
+}
+
+class Ccc private constructor(a: Int) {}
+
