09f9cad081cb9464a4ab00d1bea3b7b2112ad8af
lzp <lzp@dainihai.cn>
2017-06-30 16:55:28 +0800

    add 7 code

--- src/main/kotlin/demo/7_FunctionsAndLambdas.kt
+++ src/main/kotlin/demo/7_FunctionsAndLambdas.kt
@@ -1,5 +1,7 @@
 package demo
 
+import java.util.concurrent.locks.Lock
+
 
 fun <T> asList(vararg ts: T): List<T> {
     val result = ArrayList<T>()
@@ -8,12 +10,19 @@ fun <T> asList(vararg ts: T): List<T> {
 }
 
 tailrec fun findFixPoint(x: Double = 1.0): Double
-        = if (x==Math.cos(x)) x else findFixPoint(Math.cos(x))
+        = if (x == Math.cos(x)) x else findFixPoint(Math.cos(x))
+
+inline fun <T> lock(lock: Lock, body: () -> T) {}
+
+
 
 
 fun main(args: Array<String>) {
-    val list = asList(1,2,3,4)
-    val tmpA = arrayOf(1,2,3)
-    val listA = asList(-1,0,*tmpA, 6,7)
+    val list = asList(1, 2, 3, 4)
+    val tmpA = arrayOf(1, 2, 3)
+    val listA = asList(-1, 0, *tmpA, 6, 7)
     println(findFixPoint())
+    val sum: (Int, Int) -> Int = { x, y -> x + y }
+    val sumAdd = fun Int.(other: Int): Int = this + other
+    println(1.sumAdd(3))
 }
